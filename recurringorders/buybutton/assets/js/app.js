4c7e5bf83160a9c8112d699d112b20a9429fb7ea
Morgan Wowk <morganwowk@Morgan-Wowks-MacBook-Pro.local>
2017-04-02 20:39:40 -0500

    product selector display completed, functionality next

--- recurringorders/buybutton/assets/js/app.js
+++ recurringorders/buybutton/assets/js/app.js
@@ -1,4 +1,5 @@
 function handleProductData(data){
+	console.log(JSON.parse(data));
 	productData = JSON.parse(data);
 }
 
@@ -6,29 +7,76 @@ function getProductData(myshopify_url, page){
 	httpGetAsync("https://" + myshopify_url + "/collections/all-products/products.json?page=" + page, handleProductData);
 }
 
-function httpGetAsync(url, callback)
+function httpGetAsync(url, callback=function(){})
 {
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.onreadystatechange = function() { 
 		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
 			callback(xmlHttp.responseText);
 	}
-	xmlHttp.open("GET", url, true); // true for asynchronous 
+	xmlHttp.open("GET", url, true);
 	xmlHttp.send(null);
 }
 
-function overlay() {
-	el = document.getElementsByClassName("overlay")[0];
-	if(el.style.display == "none" || el.style.display == ""){
+function displayProductCards(page){
+
+	document.querySelector('.product-cards').innerHTML = "";
+	var keys = Object.keys(productData);
+	for(var i=(page-1)*15;i<keys.length && i<((page-1)*15)+15;i++){
+		var product_id = keys[i];
+		var product_title = productData[product_id].title;
+		var featured_image = productData[product_id].featured_image;
+		document.querySelector('.product-cards').innerHTML += 
+		document.getElementById('product-card-template').innerHTML
+		.replace('{{product_title}}', product_title)
+		.replace('{{featured_image}}', featured_image);
+	}
+
+}
+
+function getProductDataFromPage(callback){
+	httpGetAsync("https://" + myshopify_domain + "/pages/get-products", function(response){
+		productData = JSON.parse(response);
+		callback(productData);
+	});
+}
+
+function overlay(elementClass, callback) {
+	el = document.getElementsByClassName(elementClass)[0];
+	if(isHidden(el)){
 		el.style.display = "block";
 		setTimeout(function(){
-			el.style.opacity = (el.style.opacity == 0) ? "1" : "0";
+			el.style.opacity = "1";
 		}, 200);
 	}
 	else{
-		el.style.opacity = (el.style.opacity == 0) ? "1" : "0";
+		el.style.opacity = "0";
 		setTimeout(function(){
-			el.style.display = (el.style.display == "block") ? "none" : "block";
+			el.style.display = "none";
 		}, 300);
 	}
+	setTimeout(function(){
+		if(typeof(callback) == 'function'){
+			callback();
+		}
+	}, 400);
+}
+
+function isHidden(el) {
+    return (el.offsetParent === null);
+}
+
+function extractDomain(url) {
+    var domain;
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
 }
--- recurringorders/buybutton/assets/js/events.js
+++ recurringorders/buybutton/assets/js/events.js
@@ -3,5 +3,25 @@ document.querySelector(".overlay__inner").addEventListener("click", function(eve
 });
 
 document.querySelector(".overlay").addEventListener("click", function(event){
-	overlay();
+	overlay("overlay");
+});
+
+document.querySelector(".btn-go-shopify").addEventListener("click", function(event){
+	var domain = extractDomain(document.getElementById('myshopify').value);
+	if(domain.indexOf('.myshopify.com') !== -1){
+		myshopify_domain = domain;
+		overlay('who-are-you', function(){
+			overlay('loading');
+			getProductDataFromPage(function(productData){
+				overlay('loading', function(){
+					displayProductCards(1, productData);
+					overlay('product-cards-info');
+					overlay('product-cards');
+				});
+			});
+		});
+	}
+	else{
+		alert("Please enter a valid myshopify domain found in the URL of your Shopify admin.");
+	}
 });
